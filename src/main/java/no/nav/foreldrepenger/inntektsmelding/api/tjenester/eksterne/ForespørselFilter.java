935b91915c1ab74d1c79d6d0c5507d87bfb85fa4
Petter Ekern <petter.ekern@nav.no>
2026-03-10 12:53:34 +0100

    TFP-6859: Fjerner kommentar

--- src/main/java/no/nav/foreldrepenger/inntektsmelding/api/tjenester/eksterne/ForespørselFilter.java
+++ src/main/java/no/nav/foreldrepenger/inntektsmelding/api/tjenester/eksterne/ForespørselFilter.java
@@ -10,12 +10,10 @@ import jakarta.validation.constraints.Pattern;
 import no.nav.foreldrepenger.inntektsmelding.api.typer.StatusDto;
 import no.nav.foreldrepenger.inntektsmelding.api.typer.YtelseTypeDto;
 
-// I fp-inntektsmelding søker man på opprettet_dato med fom, og tom herfra, burde dette vært komunisert bedre i navn på felt?
 public record ForespørselFilter(@NotNull @Pattern(regexp = "^\\d{9}$") String orgnr,
                                 @Pattern(regexp = "^\\d{11}$") String fnr,
                                 @Valid UUID forespørselId,
                                 @Valid StatusDto status,
                                 @Valid YtelseTypeDto ytelseType,
                                 LocalDate fom,
-                                LocalDate tom) {
-}
+                                LocalDate tom) {}
