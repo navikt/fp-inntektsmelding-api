a333ac532ed6539097d22eba197b4ffe69e8b985
anjaaalerud <anja.gotesen.aalerud@nav.no>
2026-04-28 17:20:11 +0200

    TFP-6862 fjerner ubrukt import

--- src/main/java/no/nav/foreldrepenger/inntektsmelding/api/tjenester/eksterne/ForespørselRest.java
+++ src/main/java/no/nav/foreldrepenger/inntektsmelding/api/tjenester/eksterne/ForespørselRest.java
@@ -22,7 +22,6 @@ import org.slf4j.LoggerFactory;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.extensions.Extension;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
