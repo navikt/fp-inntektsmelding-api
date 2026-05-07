cdaaa58dda2d40e35031ab2d8f76b4c55b41ff27
Petter Ekern <43809623+pekern@users.noreply.github.com>
2026-05-07 21:42:56 +0200

    Chore: Legger på valideringer på utgående objekter (#70)
    * Chore: Legger på valideringer på utgående objekter
    
    * Chore: Manglende string validering

--- src/main/java/no/nav/foreldrepenger/inntektsmelding/api/inntektsmelding/InntektsmeldingDto.java
+++ src/main/java/no/nav/foreldrepenger/inntektsmelding/api/inntektsmelding/InntektsmeldingDto.java
@@ -6,43 +6,47 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotNull;
+
+import jakarta.validation.constraints.Pattern;
+
 import no.nav.foreldrepenger.inntektsmelding.api.typer.EndringsårsakDto;
 import no.nav.foreldrepenger.inntektsmelding.api.typer.NaturalytelsetypeDto;
 import no.nav.foreldrepenger.inntektsmelding.api.typer.YtelseTypeDto;
 
-public record InntektsmeldingDto(UUID inntektsmeldingId,
-                                 String soekerFnr,
-                                 YtelseTypeDto ytelse,
-                                 InntektsmeldingArbeidsgiver arbeidsgiver,
-                                 LocalDate startdato,
-                                 Inntekt inntekt,
-                                 LocalDateTime innsendtTid,
-                                 AvsenderSystem avsender,
+public record InntektsmeldingDto(@NotNull UUID inntektsmeldingId,
+                                 @NotNull @Pattern(regexp = "^\\d{11}$") String soekerFnr,
+                                 @NotNull YtelseTypeDto ytelse,
+                                 @NotNull InntektsmeldingArbeidsgiver arbeidsgiver,
+                                 @NotNull LocalDate startdato,
+                                 @NotNull Inntekt inntekt,
+                                 @NotNull LocalDateTime innsendtTid,
+                                 @NotNull AvsenderSystem avsender,
                                  Refusjon refusjon,
                                  List<Naturalytelse> naturalytelser) {
 
-    public record Inntekt(BigDecimal beloep, LocalDate inntektsdato, List<InntektEndringsårsaker> endringAarsaker) {
+    public record Inntekt(@NotNull BigDecimal beloep, @NotNull LocalDate inntektsdato, @NotNull List<InntektEndringsårsaker> endringAarsaker) {
     }
 
-    public record InntektsmeldingArbeidsgiver(String orgnr, Kontaktperson kontaktperson) {
+    public record InntektsmeldingArbeidsgiver(@NotNull @Pattern(regexp = "^\\d{9}$") String orgnr, @NotNull Kontaktperson kontaktperson) {
     }
 
-    public record Refusjon(BigDecimal beloepPerMaaned, List<RefusjonEndring> endringer) {
+    public record Refusjon(@NotNull BigDecimal beloepPerMaaned, @NotNull List<RefusjonEndring> endringer) {
     }
 
-    public record InntektEndringsårsaker(EndringsårsakDto aarsak, LocalDate fom, LocalDate tom, LocalDate bleKjentFom) {
+    public record InntektEndringsårsaker(@NotNull EndringsårsakDto aarsak, LocalDate fom, LocalDate tom, LocalDate bleKjentFom) {
     }
 
-    public record RefusjonEndring(BigDecimal beloepPerMaaned, LocalDate fom) {
+    public record RefusjonEndring(@NotNull BigDecimal beloepPerMaaned, @NotNull LocalDate fom) {
     }
 
-    public record AvsenderSystem(String systemNavn, String systemVersjon) {
+    public record AvsenderSystem(@NotNull String systemNavn, @NotNull String systemVersjon) {
     }
 
-    public record Kontaktperson(String navn, String telefonnummer) {
+    public record Kontaktperson(@NotNull String navn, @NotNull String telefonnummer) {
     }
 
-    public record Naturalytelse(BigDecimal verdi, LocalDate sluttdato, NaturalytelsetypeDto naturalytelse) {
+    public record Naturalytelse(@NotNull BigDecimal verdi, @NotNull LocalDate sluttdato, @NotNull NaturalytelsetypeDto naturalytelse) {
     }
 }
 
