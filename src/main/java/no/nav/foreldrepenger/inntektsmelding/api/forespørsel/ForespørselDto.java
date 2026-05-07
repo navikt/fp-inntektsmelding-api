b8798508f3f2e5d5c42f2f89d040a23d0e751168
Petter Ekern <petter.ekern@nav.no>
2026-05-07 16:06:45 +0200

    Chore: Legger på valideringer på utgående objekter

--- src/main/java/no/nav/foreldrepenger/inntektsmelding/api/forespørsel/ForespørselDto.java
+++ src/main/java/no/nav/foreldrepenger/inntektsmelding/api/forespørsel/ForespørselDto.java
@@ -4,10 +4,14 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotNull;
+
+import jakarta.validation.constraints.Pattern;
+
 import no.nav.foreldrepenger.inntektsmelding.api.typer.StatusDto;
 import no.nav.foreldrepenger.inntektsmelding.api.typer.YtelseTypeDto;
 
-public record ForespørselDto(UUID forespoerselId, String orgnr, String soekerFnr, LocalDate startdato,
-                             LocalDate inntektsdato, StatusDto status, YtelseTypeDto ytelseType, LocalDateTime opprettetTid) {
+public record ForespørselDto(@NotNull UUID forespoerselId, @NotNull @Pattern(regexp = "^\\d{9}$") String orgnr, @NotNull @Pattern(regexp = "^\\d{11}$") String soekerFnr, @NotNull LocalDate startdato,
+                             @NotNull LocalDate inntektsdato, @NotNull StatusDto status, @NotNull YtelseTypeDto ytelseType, @NotNull LocalDateTime opprettetTid) {
 
 }
