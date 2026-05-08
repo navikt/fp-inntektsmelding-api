4b8b854c923dbd2f984ffa8f124bfa7e9df5ef7a
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2026-05-08 12:55:58 +0200

    Chore - Feilhåndtering (#73)
    *Forbedrer beskrivelse av ErrorResponse i swagger.
    *Endret fra feilreferanse til referanseId som er det det er nå

--- src/main/java/no/nav/foreldrepenger/inntektsmelding/api/server/exceptions/ErrorResponse.java
+++ src/main/java/no/nav/foreldrepenger/inntektsmelding/api/server/exceptions/ErrorResponse.java
@@ -7,9 +7,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Feilrespons returnert ved feil")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
-    @Schema(description = "Maskinlesbar feilkode", example = "TOM_FORESPOERSEL") String feilkode,
-    @Schema(description = "Beskrivelse av feilen", example = "Finner ikke forespørsel") String feilmelding,
-    @Schema(description = "Referanse-ID for feilsøking (callId)", nullable = true) String feilreferanse) {
+    @Schema(description = "Maskinlesbar feilkode", example = "FEIL_KODE") String feilkode,
+    @Schema(description = "Beskrivelse av feilen", example = "Beskrivelse av feilen") String feilmelding,
+    @Schema(description = "ForespørselId eller InntektsmeldingId på den som feilet", nullable = true) String referanseId) {
 
     public ErrorResponse(String feilkode, String feilmelding) {
         this(feilkode, feilmelding, null);
