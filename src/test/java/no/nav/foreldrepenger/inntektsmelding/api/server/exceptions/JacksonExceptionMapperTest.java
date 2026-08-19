95c46b1c81fb0d9c54aa88e9e524a3e1cbcaed13
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-08-19 08:46:18 +0200

    TFP-6470 felles 7.9.0 - standard jackson3 (#121)

--- src/test/java/no/nav/foreldrepenger/inntektsmelding/api/server/exceptions/JacksonExceptionMapperTest.java
+++ src/test/java/no/nav/foreldrepenger/inntektsmelding/api/server/exceptions/JacksonExceptionMapperTest.java
@@ -4,14 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-import no.nav.foreldrepenger.inntektsmelding.api.server.app.api.Jackson3ExceptionMapper;
+import no.nav.foreldrepenger.inntektsmelding.api.server.app.api.JacksonExceptionMapper;
 import tools.jackson.databind.exc.InvalidTypeIdException;
 
-class Jackson3ExceptionMapperTest {
+class JacksonExceptionMapperTest {
 
     @Test
     void skal_mappe_InvalidTypeIdException() {
-        var mapper = new Jackson3ExceptionMapper();
+        var mapper = new JacksonExceptionMapper();
         try (var resultat = mapper.toResponse(new InvalidTypeIdException(null, "Ukjent type-kode", null, "23525"))) {
             var dto = (ErrorResponse) resultat.getEntity();
             assertThat(dto.feilmelding()).startsWith("Serialiseringsfeil: Ukjent type-kode");
